a5db771ef4f9cbc2caa3523f72c38c71cb6b3302
Amine Benelbaida <jitare@live.fr>
2019-02-10 10:43:51 +0100

    First Commit project setup 10/02/2019

---
+++ shoppingbackend/src/main/java/com/abe/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.abe.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
